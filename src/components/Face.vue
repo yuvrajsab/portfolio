01f3950cb28d8f9dca95328323f0d09908fd6ca4
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-11-09 19:58:53 +0530

    add myface component

---
+++ src/components/Face.vue
@@ -0,0 +1,19 @@
+<template>
+    <img src="../assets/logo.png" class="bg-dark rounded mx-auto shadow" alt="MyFace">
+</template>
+
+<script>
+export default {
+    props: {
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    img {
+        display: block;
+        width: 100%;
+        max-width: 160px;
+        height: auto;
+    }
+</style>
