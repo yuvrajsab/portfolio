1474a6d93b103a0c5c1f65494ddbe98d4208a098
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-11-09 20:03:33 +0530

    design homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-3 col-md-4 py-3">
+        <div class="container">
+          <Face />
+        </div>
+        <SocialLinks />
+      </div>
+      <div v-show="!isMobile" class="border-right pl-4"></div>
+      <div class="col-lg-6 col-md-7 py-3">
+        <About />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Face from '@/components/Face.vue';
+import SocialLinks from '@/components/SocialLinks.vue';
+import About from '@/components/About.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Face,
+    About,
+    SocialLinks,
+  },
+  data() {
+    return {
+      isMobile: window.innerWidth < 768 ? true : false,
+    }
   }
 }
 </script>
