9f7f43db64457ff82924a98de8790a5530ce3c5f
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-11-09 20:29:19 +0530

    change aboutme lilbit

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,19 +3,22 @@
         <h1 class="text-monospace">Yuvraj Sablania</h1>
         <div>
             <p>
-                Hello World ! I'm currently pursuing Masters in Computer Applications at University of Delhi. I'm passionate about web and software development.
+                Hello, I live in New Delhi, India.
             </p>
             <p>
-                I'm open to job offers! Here's my Resume.
+                Currently, I'm pursuing Masters in Computer Applications at University of Delhi and I'm passionate about web and software development.
             </p>
             <p>
-                If you're interested in knowing my technical experience, checkout my Projects. 
+                If you're interested in knowing my technical experience, checkout my  
+                <button class="btn btn-primary btn-sm">
+                    Projects
+                    <i class="icon ion-md-arrow-forward"></i>
+                </button>
+            </p>
+            <p>
+                I’m always open to exciting new project opportunities. Please get in touch, it would be great to hear from you.
             </p>
         </div>
-        <button class="btn btn-primary btn-sm">
-            Projects
-            <i class="icon ion-md-arrow-forward"></i>
-        </button>
     </div>
 </template>
 
